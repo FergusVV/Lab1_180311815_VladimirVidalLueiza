2d525fe5e893af513c1233fffad6fc31422c7d44
Vladimir Vangelis Vidal Lueiza <vladimir.vidal@usach.cl>
2024-04-22 02:44:36 -0400

    documentacion tda line y pcar

--- line_180311815_vladimirvidallueiza.rkt
+++ line_180311815_vladimirvidallueiza.rkt
@@ -4,143 +4,27 @@
 (require "station_180311815_vladimirvidallueiza.rkt")
 (require "section_180311815_vladimirvidallueiza.rkt")
 
+; TDA Line.
+; Representada por una lista con
+; id (int) X name (string) X rail-type(string)Xsection* (* señala que se pueden agregar 0 o más tramos)
+
+;---------------------Constructor---------------------
+; Crea una línea de metro con un identificador, nombre, tipo de riel y una lista opcional de secciones.
+; Dominio: iid (int) X name (string) X rail-type(string) X section  * (* señala que se pueden agregar 0 o más tramos)
+; Recorrido: line
 (define line
   (lambda (id name rail-type . sections)
-    (if (and (integer? id)
-             (string? name)
-             (string? rail-type)
-             (andmap is-section? sections))  ; Asegura que todos los elementos en 'sections' son secciones válidas.
-        (list id name rail-type sections)  ; Envuelve 'sections' en una lista para mantener una estructura consistente.
-       (raise "Invalid line parameters"))))
+    (if (and (integer? id)  ; Verifica que el id sea un entero.
+             (string? name)  ; Verifica que name sea una cadena de texto.
+             (string? rail-type)  ; Verifica que rail-type sea una cadena de texto.
+             (andmap section? sections))  ; Asegura que todos los elementos en 'sections' son secciones válidas.
+        (list id name rail-type sections)  ; Construye la línea con las secciones envueltas en una lista.
+        (raise "Invalid line parameters"))))
 
-(define (es-line? line)
-  (and (list? line)
-       (integer? (get-line-id line))
-       (string? (get-line-name line))
-       (string? (get-line-rail-type line))
-       (list? (get-line-sections line))  ; Asegura que las secciones son una lista.
-       (andmap is-section? (get-line-sections line)))) 
-
-(define get-line-id
-  (lambda (line)
-    (first line)))
-
-(define get-line-name
-  (lambda (line)
-    (second line)))
-
-(define get-line-rail-type
-  (lambda (line)
-    (third line)))
-
-(define get-line-sections
-  (lambda (line)
-    (if (>= (length line) 4)
-        (fourth line)
-        '())))
-
-;;otras funciones
-;;line-length
-;;comprueba si una linea es circular
-(define (line-is-circular? line)
-  (and (not (null? (get-line-sections line)))
-       (equal? (get-station-id (get-section-point1 (first (get-line-sections line))))
-               (get-station-id (get-section-point2 (last (get-line-sections line)))))))
-
-
-(define (regular-line-length line)
-  (apply + (map get-section-distance (get-line-sections line))))
-
-(define (circular-line-length line)
-  (apply + (map get-section-distance (get-line-sections line))))
-
-
-(define (line-length line)
-  (if (line-is-circular? line)
-      (circular-line-length line)
-      (regular-line-length line)))
-
-;;Esta función helper es una función interna que utiliza recursión para acumular la distancia.
-;;La variable acum es un indicador booleano que comienza como #f (falso) y se vuelve #t (verdadero) cuando encontramos la estación de inicio.
-;;Esto nos permite saber si debemos sumar las distancias de las secciones subsecuentes o no.
-;;La función helper se llama inicialmente con la lista de secciones de la línea y #f para indicar que todavía no estamos acumulando la distancia.
-(define (line-section-length line start-station-name end-station-name)
-  (define (calculate-distance sections acum)
-    (cond
-      ((null? sections) 0)  ; No hay más secciones para procesar.
-      ((string=? (get-station-name (get-section-point2 (first sections))) end-station-name) ;; Encontró la estación de fin, sumar esta distancia si está acumulando.
-       (if acum (get-section-distance (first sections)) 0))
-      ((string=? (get-station-name (get-section-point1 (first sections))) start-station-name) ;; Encontró la estación de inicio, sumar esta distancia y las subsiguientes.
-       (+ (get-section-distance (first sections)) 
-          (calculate-distance (rest-of-list sections) #t)))
-      (acum
-       ;; Si ya se ha comenzado a acumular, sumar esta distancia.
-       (+ (get-section-distance (first sections)) 
-          (calculate-distance (rest-of-list sections) #t)))
-      (else
-       ;; Si no se ha encontrado ninguna estación relevante, o aún no se acumula, continuar con la siguiente sección.
-       (calculate-distance (rest-of-list sections) acum))))
-  (calculate-distance (get-line-sections line) #f))
-
-;line-cost
-(define (line-cost line)
-  (define (calculate-total-cost sections)
-    (if (null? sections)
-        0  ; No hay más secciones, no hay costo para calcular.
-        (+ (get-section-cost (first sections))  ; Sumar el costo de la sección actual.
-           (calculate-total-cost (rest-of-list sections)))))  ; Recursivamente sumar el costo de las secciones restantes.
-  (calculate-total-cost (get-line-sections line)))
-
-;;;line-section-cost
-(define (line-section-cost line start-station-name end-station-name)
-  (define (calculate-cost sections accumulated-cost start-found)
-    (if (null? sections)
-        accumulated-cost  ; Retorna el costo acumulado, finalizando la recursión.
-        (cond
-          ((string=? (get-station-name (get-section-point2 (first sections))) end-station-name)
-           (if start-found
-               (+ accumulated-cost (get-section-cost (first sections)))  ; Agrega el costo final y retorna.
-               (calculate-cost (rest-of-list sections) accumulated-cost start-found)))  ; No ha comenzado a acumular, sigue buscando.
-          ((string=? (get-station-name (get-section-point1 (first sections))) start-station-name)
-           (calculate-cost (rest-of-list sections) (+ accumulated-cost (get-section-cost (first sections))) #t))  ; Comienza a acumular.
-          (start-found
-           (calculate-cost (rest-of-list sections) (+ accumulated-cost (get-section-cost (first sections))) start-found))  ; Continúa acumulando.
-          (else
-           (calculate-cost (rest-of-list sections) accumulated-cost start-found)))))  ; No se acumula aún, sigue buscando.
-  (calculate-cost (get-line-sections line) 0 #f))
-
-
-
-;;line-add-section
-(define crearline
-  (lambda (id name rail-type sections)
-    (if (and (integer? id)
-             (string? name)
-             (string? rail-type)
-             ;;(is-section? sections)
-             )  ; Asegura que todos los elementos en 'sections' son secciones válidas.
-        (list id name rail-type sections)  ; Envuelve 'sections' en una lista para mantener una estructura consistente.
-        (raise "Invalid line parameters crear line"))))
-
-(define (line-add-section line section)
-  (define (section-exists? sections section)
-    (cond ((null? sections) #f)  ; No hay más secciones, la sección no está duplicada.
-          ((equal? (car sections) section) #t)  ; Se encontró una sección duplicada.
-          (else (section-exists? (cdr sections) section))))  ; Recursión para verificar el resto.
-
-  (define (add-section sections section)
-    (if (section-exists? sections section)
-        sections  ; Si la sección está duplicada, retorna las secciones sin cambios.
-        (append sections (list section))))  ; Añade la sección al final.
-
-  (if (is-section? section)
-      (crearline (get-line-id line)
-            (get-line-name line)
-            (get-line-rail-type line)
-            (add-section (get-line-sections line) section))
-      (error "Invalid section to add")))
-
-;;Comprobar si es linea
+;---------------------Pertenencia---------------------
+; Verifica si un objeto es una línea válida.
+; Dominio: line
+; Recorrido: boolean
 (define (line? line)
   (and (list? line)  ; Debe ser una lista.
        (= (length line) 4)  ; Debe tener exactamente cuatro elementos.
@@ -164,8 +48,166 @@
 (define (all-sections-valid? sections)
   (if (null? sections)
       #t
-      (and (is-section? (first sections))
+      (and (section? (first sections))
            (all-sections-valid? (rest-of-list sections)))))
+
+;---------------------Selectores---------------------
+
+; get-line-id: Devuelve el ID de la línea.
+; Dominio: line
+; Recorrido: int
+(define get-line-id
+  (lambda (line)
+    (first-of-list line)))
+
+; get-line-name: Devuelve el nombre de la línea.
+; Dominio: line
+; Recorrido: string
+(define get-line-name
+  (lambda (line)
+    (second-of-list line)))
+
+; get-line-rail-type: Devuelve el tipo de riel de la línea.
+; Dominio: line
+; Recorrido: string
+(define get-line-rail-type
+  (lambda (line)
+    (third-of-list line)))
+
+; get-line-sections: Devuelve las secciones de la línea, o una lista vacía si no hay secciones.
+; Dominio: line
+; Recorrido: list of section
+(define get-line-sections
+  (lambda (line)
+    (if (>= (length line) 4)
+        (fourth-of-list line)
+        '())))
+
+;---------------------Otras Funciones---------------------
+; line-length: Calcula la longitud total de una línea, diferenciando entre líneas circulares y no circulares.
+; No se usa recursividad directamente
+; Dominio: line
+; Recorrido: positive-number
+(define (line-length line)
+  (if (line-is-circular? line)
+      (circular-line-length line)
+      (regular-line-length line)))
+
+; Funciones para calcular la longitud de líneas regulares y circulares.
+; Dominio: line
+; Recorrido: number
+(define (regular-line-length line)
+  (apply + (map get-section-distance (get-line-sections line))))
+
+(define (circular-line-length line)
+  (apply + (map get-section-distance (get-line-sections line))))
+
+; Función para verificar si una línea es circular.
+; Verifica que la primera estación de la primera sección y la última estación de la última sección sean las mismas.
+; Dominio: line
+; Recorrido: boolean
+(define (line-is-circular? line)
+  (and (not (null? (get-line-sections line)))
+       (equal? (get-station-id (get-section-point1 (first (get-line-sections line))))
+               (get-station-id (get-section-point2 (last (get-line-sections line)))))))
+
+
+;------------------------------------------
+
+; line-section-length: calcular la longitud de la sección de una línea entre dos estaciones dadas.
+; Dominio: line (line) X station1-name (String) X station2-name (String)
+; Recorrido: positive-number
+; Uso de recursión
+(define (line-section-length line start-station-name end-station-name)
+  ;; Función recursiva interna para calcular la distancia.
+  ;; sections es la lista de secciones de la línea, y acum es un booleano para indicar si estamos sumando distancias.
+  (define (calculate-distance sections acum)
+    ;; Caso base: si la lista de secciones está vacía, retorna 0.
+    (if (null? sections)
+        0
+        ;; Evalúa la primera sección de la lista.
+        (cond
+          ;; Si la segunda estación de la sección actual es la estación final, verifica si estamos acumulando.
+          ((string=? (get-station-name (get-section-point2 (first-of-list sections))) end-station-name)
+           ;; Si acum es verdadero, suma la distancia de esta sección; si no, retorna 0.
+           (if acum
+               (get-section-distance (first-of-list sections))
+               0))
+          ;; Si la primera estación de la sección actual es la estación de inicio o ya estamos acumulando,
+          ;; suma la distancia de esta sección y continua con las siguientes secciones.
+          ((or (string=? (get-station-name (get-section-point1 (first-of-list sections))) start-station-name)
+               acum)
+           (+ (get-section-distance (first-of-list sections))
+              (calculate-distance (rest-of-list sections) #t)))
+          ;; En cualquier otro caso, simplemente continúa con las siguientes secciones sin acumular.
+          (else
+           (calculate-distance (rest-of-list sections) acum)))))
+  ;; Inicia la función recursiva con la lista completa de secciones de la línea y `#f` indicando que aún no se acumula distancia.
+  (calculate-distance (get-line-sections line) #f))
+
+;------------------------------------------
+; line-cost : Calcula el costo total de todas las secciones de una línea.
+; Dominio: line
+; Recorrido: number
+; Recursión natural porque estan pendientes en cada iteración.
+(define (line-cost line)
+  (define (calculate-total-cost sections)
+    (if (null? sections)
+        0
+        (+ (get-section-cost (first sections))
+           (calculate-total-cost (rest-of-list sections)))))
+  (calculate-total-cost (get-line-sections line)))
+
+;------------------------------------------
+
+; line-section-cost: Función para calcular el costo de una sección específica entre dos estaciones dentro de una línea.
+; Dominio: line X start-station-name(string) X end-station-name(string)
+; Recorrido: positive-number U {0}
+; Uso de recursíon de cola ya que no deja estados pendiente en cada llamado
+;; Define una función para calcular el costo acumulado de viajar entre dos estaciones en una línea.
+(define (line-section-cost line start-station-name end-station-name)
+  ;; Función interna recursiva de cola para calcular el costo.
+  ;; Utiliza acum-cost para mantener el costo acumulado y start-found para indicar si la estación de inicio ha sido encontrada.
+  (define (calculate-cost sections acum start-found)
+    (if (null? sections)
+        acum  ; Si no hay más secciones, retorna el costo acumulado.
+        (cond
+          ;; Si se encuentra la estación final y la estación de inicio ha sido encontrada.
+          ((and start-found (string=? (get-station-name (get-section-point2 (first-of-list sections))) end-station-name))
+           (+ acum (get-section-cost (first sections))))  ; Suma y retorna el costo de esta última sección.
+          ;; Si se encuentra la estación de inicio o si ya se ha comenzado a acumular.
+          ((or (string=? (get-station-name (get-section-point1 (first-of-list sections))) start-station-name)
+               start-found)
+           (calculate-cost (rest-of-list sections) (+ acum (get-section-cost (first sections))) #t))  ; Continúa con el costo actualizado.
+          ;; Continúa sin actualizar el costo acumulado si no se han encontrado condiciones relevantes.
+          (else
+           (calculate-cost (rest-of-list sections) acum start-found)))))
+  ;; Inicia la función recursiva con la lista completa de secciones de la línea, un costo acumulado de 0 y sin haber encontrado la estación de inicio.
+  (calculate-cost (get-line-sections line) 0 #f))
+
+
+;------------------------------------------
+
+; Función line-add-section: Añade una sección a una línea si no está ya presente.
+; Dominio: line X section
+; Recorrido: line
+; Recursión natural
+(define (line-add-section line section)
+  (if (not (section? section))
+      (raise "Invalid section to add")
+      (list (get-line-id line)
+            (get-line-name line)
+            (get-line-rail-type line)
+            (let add-if-not-exists ((sections (get-line-sections line)))
+              (cond ((null? sections) (list section))
+                    ((equal? (car sections) section) sections)
+                    (else (cons (car sections) (add-if-not-exists (cdr sections)))))))))
+
+
+(define (crearline id name rail-type sections)
+  (list id name rail-type sections))
+
+
 
 
 
--- pcar_180311815_vladimirvidallueiza.rkt
+++ pcar_180311815_vladimirvidallueiza.rkt
@@ -1,30 +1,71 @@
 #lang racket
-
 (provide (all-defined-out))
 
-(define tr 'tr)  ; Definicion previa como un simbolo
-(define ct 'ct)
+;; Definición de símbolos para los tipos de carro.
+(define tr 'tr)  ; Terminal: Estaciones finales de una línea y que pueden operar como combinaciones con otras líneas o servicios.
+(define ct 'ct)  ; Central: Carros que operan en el centro del convoy.
+; Renombrar las funciones selectoras de listas.
+(define first-of-list car) ; Obtiene el primer elemento de una lista.
+(define second-of-list second) ; Obtiene el segundo elemento de una lista.
+(define third-of-list third) ; Obtiene el tercer elemento de una lista.
+(define fourth-of-list fourth) ; Obtiene el cuarto elemento de una lista.
+(define rest-of-list cdr) ; Elimina el primer elemento y devuelve el resto de la lista.
 
-; Define el constructor para un carro de pasajero
-;Los carros pueden ser de ti poterminal (tr) o central (ct)
+; TDA Pcar.
+; Representada por una lista con
+; id (int) X capacity (positive integer) X model (string) X type (car-type)
+
+;---------------------Constructor---------------------
+
+;; pcar:Constructor para un carro de pasajero.
+;; Los carros pueden ser de tipo terminal (tr) o central (ct).
+;; Dominio: id (int), capacity (positive integer), model (string), type (tr or ct).
+;; Recorrido: pcar
 (define pcar
   (lambda (id capacity model type)
-    (if (and (integer? id)                   ; Verifica que el ID sea un entero.
-             (positive? capacity)            ; Verifica que la capacidad sea un número positivo.
-             (string? model)                 ; Verifica que el modelo sea una cadena de texto.
-             (member type (list tr ct)))     ; Verifica que el tipo sea 'tr' o 'ct', usando símbolos definidos.
-        (list id capacity model type)        ; Crea una lista con los detalles del carro.
-        (error "Invalid pcar parameters")))) ; Lanza un error si algún parámetro no es válido.
+    (if (and (integer? id)                    ; Verifica que el ID sea un entero.
+             (positive? capacity)             ; Verifica que la capacidad sea un número positivo.
+             (string? model)                  ; Verifica que el modelo sea una cadena de texto.
+             (member type (list tr ct)))      ; Verifica que el tipo sea 'tr' o 'ct'.
+        (list id capacity model type)         ; Crea y retorna una lista con los detalles del carro.
+        (error "Invalid pcar parameters"))))  ; Lanza un error si algún parámetro no es válido.
 
-(define (pcar-id pcar)
-  (first pcar))
 
-(define (pcar-capacity pcar)
-  (second pcar))
+;---------------------Pertenencia---------------------
+;; Función de pertenencia para pcar.
+;; Verifica que un objeto sea un carro de pasajero válido según la definición del constructor.
+;; Dominio: pcar (lista).
+;; Recorrido: booleano.
+(define (pcar? pcar)
+  (and (list? pcar)                           ; Verifica que pcar sea una lista.
+       (= (length pcar) 4)                    ; Verifica que pcar tenga cuatro elementos.
+       (integer? (get-pcar-id pcar))              ; Verifica que el ID sea un entero.
+       (positive? (get-pcar-capacity pcar))       ; Verifica que la capacidad sea un número positivo.
+       (string? (get-pcar-model pcar))            ; Verifica que el modelo sea una cadena de texto.
+       (member (get-pcar-type pcar) (list tr ct))))  ; Verifica que el tipo sea 'tr' o 'ct'.
 
-(define (pcar-model pcar)
-  (third pcar))  ; Asume que el modelo está en la tercera posición.
+;---------------------Selectores---------------------
 
-; Define la función para extraer el tipo de un carro.
-(define (pcar-type pcar)
-  (fourth pcar))
+;; Selector para el ID del carro.
+;; Dominio: pcar.
+;; Recorrido: ID del carro (int).
+(define (get-pcar-id pcar)
+  (first-of-list pcar))
+
+;; Selector para la capacidad del carro.
+;; Dominio: pcar.
+;; Recorrido: capacidad del carro (positive integer).
+(define (get-pcar-capacity pcar)
+  (second-of-list pcar))
+
+;; Selector para el modelo del carro.
+;; Dominio: pcar.
+;; Recorrido: modelo del carro (string).
+(define (get-pcar-model pcar)
+  (third-of-list pcar))
+
+;; Selector para el tipo del carro.
+;; Dominio: pcar.
+;; Recorrido: tipo del carro (tr o ct).
+(define (get-pcar-type pcar)
+  (fourth-of-list pcar))
